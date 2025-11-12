c59407ccf84ef341885a69f982aa29011f39bf19
axpknickei <alexprima@student.ub.ac.id>
2025-11-12 20:15:05 +0700

    6. correcting the return of contents in  function. Returning saved variable  in  function

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-file_contents = ""
-
 def get_book_text(file_path):
     with open(file_path) as f:
-        file_contents = f.read()    
-        return file_contents
+        contents = f.read()
+    return contents
 
 def main():
-    file_path = "/books/frankenstein.txt"
-    get_book_text(file_path)
+    file_path = "./books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
     print(file_contents)
 
 main()
