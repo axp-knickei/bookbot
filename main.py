bf627904fc14ef1eee37feb695eb6eebe1899673
axpknickei <alexprima@student.ub.ac.id>
2025-11-12 18:58:21 +0700

    5. Adding empty string file_contents

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-# How to do for this step
+file_contents = ""
 
-# with Block
-#with open(path_to_file) as f:
-    # do something with f (the file) here
-
-# The `with` block will automatically close the file when the block is exited
-
-# .read() Method
-# You can use the `.read()` method to read the contents of a file into a string
-## f is a file object
-#file_contents = f.read()
-
-def get_book_text(file_path): # it takes fileparth as input and returns the contents of the file as a string
+def get_book_text(file_path):
     with open(file_path) as f:
-        file_contents = f.read()
-        print(file_contents)
+        file_contents = f.read()    
+        return file_contents
 
 def main():
     file_path = "/books/frankenstein.txt"
+    get_book_text(file_path)
+    print(file_contents)
 
 main()
-
-
